9b6380424add7b9a24003e4f29d084137f1b4785
jack <254184504+im-ashey@users.noreply.github.com>
2026-09-01 21:00:41 -0700

    alright! i got the form to render!

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,55 @@
-function checkStatus() {
+function main() {
     const apiKey = localStorage.getItem('apiKey');
     const deviceID = localStorage.getItem('deviceID');
 
     if (apiKey && deviceID) {
         console.log('items present');
     } else {
-        presentSetup();
+        renderSetup();
     }
 }
 
-function presentSetup() {
+function renderSetup() {
     let containerDOM = document.querySelector('div#setupContainer');
 
     const headerElement = document.createElement('h2');
-    const headerElementText = document.createTextNode('govee lights setup');
+    const headerElementText = document.createTextNode('setup your lights');
 
+    function renderInputs(name, textContent, type, end) {
+        const inputElement = document.createElement('input');
+        const labelElement = document.createElement('label');
+        const breakElement = document.createElement('br');
+        const buttonElement = document.createElement('button');
+        function insertElement(element) { containerDOM.appendChild(element) };
+
+        labelElement.htmlFor = name;
+        labelElement.textContent = textContent;
+
+        inputElement.name = name;
+        inputElement.type = type;
+        inputElement.id = name;
+
+        insertElement(labelElement);
+        insertElement(inputElement);
+
+        if (end) {
+            buttonElement.id = 'submitInformation';
+            buttonElement.textContent = 'submit';
+
+            insertElement(breakElement)
+            insertElement(buttonElement)
+        } else {
+            insertElement(breakElement);
+        }
+    }
+
+    // generate the header
     headerElement.appendChild(headerElementText);
-
     containerDOM.appendChild(headerElement);
+
+    // lets generate the first form
+    renderInputs('apiKeyInput', 'api key: ', 'password', false);
+
+    // second form, now.
+    renderInputs('deviceIDInput', 'device ID: ', 'text', true);
 }
