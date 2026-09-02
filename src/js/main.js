8fd0d7b0f23132b823b139a73ce9700bd30c6d1d
jack <254184504+im-ashey@users.noreply.github.com>
2026-09-01 18:56:03 -0700

    starts the setup code

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-console.log('hello, world')
+function checkStatus() {
+    const apiKey = localStorage.getItem('apiKey');
+    const deviceID = localStorage.getItem('deviceID');
+
+    if (apiKey && deviceID) {
+        console.log('items present');
+    } else {
+        presentSetup();
+    }
+}
+
+function presentSetup() {
+    let containerDOM = document.querySelector('div#setupContainer');
+
+    const headerElement = document.createElement('h2');
+    const headerElementText = document.createTextNode('govee lights setup');
+
+    headerElement.appendChild(headerElementText);
+
+    containerDOM.appendChild(headerElement);
+}
