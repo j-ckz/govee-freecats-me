60963c86947f48db5a3f6c632db949d553927cfb
jack <254184504+im-ashey@users.noreply.github.com>
2026-09-01 21:26:14 -0700

    awesome sauce, i just finished setting up the listener, so now i can use the inputs somewhere.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
-function main() {
+window.addEventListener("load", (event) => {
+    checkForData();
+});
+
+function checkForData() {
     const apiKey = localStorage.getItem('apiKey');
     const deviceID = localStorage.getItem('deviceID');
 
@@ -6,11 +10,12 @@ function main() {
         console.log('items present');
     } else {
         renderSetup();
+        listenForInputs();
     }
 }
 
 function renderSetup() {
-    let containerDOM = document.querySelector('div#setupContainer');
+    let containerDOM = document.querySelector('#setupContainer');
 
     const headerElement = document.createElement('h2');
     const headerElementText = document.createTextNode('setup your lights');
@@ -36,8 +41,8 @@ function renderSetup() {
             buttonElement.id = 'submitInformation';
             buttonElement.textContent = 'submit';
 
-            insertElement(breakElement)
-            insertElement(buttonElement)
+            insertElement(breakElement);
+            insertElement(buttonElement);
         } else {
             insertElement(breakElement);
         }
@@ -52,4 +57,32 @@ function renderSetup() {
 
     // second form, now.
     renderInputs('deviceIDInput', 'device ID: ', 'text', true);
+}
+
+function listenForInputs() {
+    const buttonDOM = document.getElementById('submitInformation');
+
+    buttonDOM.addEventListener("click", () => {
+        const apiKeyInputDOM = document.getElementById('apiKeyInput');
+        const deviceIDInputDOM = document.getElementById('deviceIDInput');
+
+        const apiKey = btoa(apiKeyInputDOM.value);
+        const deviceID = deviceIDInputDOM.value;
+
+        if (apiKey !== '' && deviceID !== '') {
+            localStorage.setItem('apiKey', apiKey);
+            localStorage.setItem('deviceID', deviceID);
+
+            location.reload();
+        } else {
+            if (apiKey === '') {
+                alert('your API key input was left empty. this is a required input, so please fill it out.');
+            } else if (deviceID === '') {
+                alert('your device ID input was left empty. this is a required input, so please fill it out.');
+            } else {
+                alert('what the fuck?');
+            }
+        }
+
+    });
 }
