baf91abcc00026cde5a84735b545d7ca9f1a1eca
jack <254184504+im-ashey@users.noreply.github.com>
2026-09-02 18:20:07 -0700

    implements power on and off and implements brightness stuff

--- src/js/main.js
+++ src/js/main.js
@@ -11,24 +11,16 @@ function checkForData() {
 
     if (apiKey && deviceID) {
         renderControlPanel();
+        listenForInputs('controlPanel')
     } else {
         renderSetup();
-        listenForInputs();
+        listenForInputs('setupScreen');
     }
 }
 
 function renderControlPanel() {
 
     /*
-    power:
-    <button (pwron)>; <button (pwroff)>
-
-    brightness:
-    <label>; <input>; <button/submit>;
-
-    color:
-    <label>; <input>; <button/submit>;
-
     i should maybe add something that can change the device ID on the side and/or show all the device IDs for easy switching. devids with like the product name or somehting.
     */
 
@@ -79,26 +71,11 @@ function renderControlPanel() {
     insertElement(document.createElement('br'));
     createInput('brightnessInput', 'brightness: ', 'range');
     createInput('colorInput', 'color: ', 'color');
-
 }
 
 /*
 
-const apiHost = 'https://openapi.api.govee.com';
-const contentType = 'application/json';
-const apiKey = localStorage.getItem('apiKey');
 
-fetch(`${apiHost}/router/api/v1/user/devices`, {
-    headers: {
-        'Govee-API-Key': atob(apiKey),
-        'Content-Type': contentType
-    }
-})
-.catch(error => console.log('Error while fetching:', error))
-.then(response => response.json())
-.then((response) => {
-    console.log(response)
-})
 
  */
 
@@ -144,30 +121,117 @@ function renderSetup() {
     createInput('deviceIDInput', 'device ID: ', 'text', true);
 }
 
-function listenForInputs() {
-    const buttonDOM = document.getElementById('submitInformation');
+function submitData(type, value) {
+    const apiHost = 'https://openapi.api.govee.com/router/api/v1/device/control';
+    const contentType = 'application/json';
+    const apiKey = localStorage.getItem('apiKey');
+    const deviceID = localStorage.getItem('deviceID')
+    let requestBody;
 
-    buttonDOM.addEventListener("click", () => {
-        const apiKeyInputDOM = document.getElementById('apiKeyInput');
-        const deviceIDInputDOM = document.getElementById('deviceIDInput');
-
-        const apiKey = btoa(apiKeyInputDOM.value);
-        const deviceID = deviceIDInputDOM.value;
-
-        if (apiKey !== '' && deviceID !== '') {
-            localStorage.setItem('apiKey', apiKey);
-            localStorage.setItem('deviceID', deviceID);
-
-            location.reload();
-        } else {
-            if (apiKey === '') {
-                alert('your API key input was left empty. this is a required input, so please fill it out.');
-            } else if (deviceID === '') {
-                alert('your device ID input was left empty. this is a required input, so please fill it out.');
-            } else {
-                alert('what the fuck?');
+    if (type === 'brightness') {
+        requestBody = JSON.stringify({
+            requestId: crypto.randomUUID(),
+            payload: {
+                "sku": "H618F",
+                "device": deviceID,
+                "capability": {
+                    "type": "devices.capabilities.range",
+                    "instance": 'brightness',
+                    "value": Number(value)
+                }
             }
-        }
+        })
+    } else if (type === 'powerOnOff') {
+        requestBody = JSON.stringify({
+            requestId: crypto.randomUUID(),
+            payload: {
+                "sku": "H618F",
+                "device": deviceID,
+                "capability": {
+                    "type": "devices.capabilities.on_off",
+                    "instance": 'powerSwitch',
+                    "value": value
+                }
+            }
+        })
+    }
+    fetch(apiHost, {
+        method: "POST",
+        headers: {
+            'Govee-API-Key': atob(apiKey),
+            'Content-Type': contentType
+        },
+        body: requestBody
+    })
 
-    });
+        /*
+
+{
+  "requestId": "1",
+  "payload": {
+    "sku": "H605C",
+    "device": "64:09:C5:32:37:36:2D:13",
+    "capability": {
+      "type": "devices.capabilities.range",
+      "instance": "brightness",
+      "value": 50
+    }
+  }
+} */
+    .catch(error => console.log('Error while fetching:', error))
+    .then(response => response.json())
+
+    console.log(type, value);
+}
+
+function listenForInputs(type) {
+    if (type === 'setupScreen') {
+        const buttonDOM = document.getElementById('submitInformation');
+
+        buttonDOM.addEventListener("click", () => {
+            const apiKeyInputDOM = document.getElementById('apiKeyInput');
+            const deviceIDInputDOM = document.getElementById('deviceIDInput');
+
+            const apiKey = btoa(apiKeyInputDOM.value);
+            const deviceID = deviceIDInputDOM.value;
+
+            if (apiKey !== '' && deviceID !== '') {
+                localStorage.setItem('apiKey', apiKey);
+                localStorage.setItem('deviceID', deviceID);
+
+                location.reload();
+            } else {
+                if (apiKey === '') {
+                    alert('your API key input was left empty. this is a required input, so please fill it out.');
+                } else if (deviceID === '') {
+                    alert('your device ID input was left empty. this is a required input, so please fill it out.');
+                } else {
+                    alert('what the fuck?');
+                }
+            }
+
+        });
+    } else if (type === 'controlPanel') {
+        const powerOnDOM = document.getElementById('poweronButton');
+        const powerOffDOM = document.getElementById('poweroffButton');
+        const brightnessSubmitDOM = document.getElementById('brightnessInputSubmit');
+        const colorSubmitDOM = document.getElementById('colorInputSubmit');
+
+        const brightnessDOM = document.getElementById('brightnessInput');
+        const colorDOM = document.getElementById('colorInput');
+
+        powerOnDOM.addEventListener('click', () => {
+            submitData('powerOnOff', 1);
+        });
+        powerOffDOM.addEventListener('click', () => {
+            submitData('powerOnOff', 0);
+        });
+
+        brightnessSubmitDOM.addEventListener('click', () => {
+            const brightness = brightnessDOM.value;
+            submitData('brightness', brightness);
+        });
+
+        colorSubmitDOM.addEventListener('click', () => { });
+    }
 }
